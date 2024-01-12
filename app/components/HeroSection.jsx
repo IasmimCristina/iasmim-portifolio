2abe5b9ac0808dfb389aa16e7de46188b453c87d
Iasmim Pereira <100351576+IasmimCristina@users.noreply.github.com>
2024-01-11 22:11:29 -0300

    feat: created initial versionheader.

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+    <h1 className='text-[#ADDFDE]'>Portfolio - Iasmim</h1>
+    </section>
+  )
+}
+
+export default HeroSection;
